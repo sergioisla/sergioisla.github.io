f7404e459d0586dfa1a61ee8e0f96d8d005b8142
Sergio Isla <122633667+sergioisla@users.noreply.github.com>
2023-08-21 10:14:13 -0600

    Add files via upload

---
+++ app/page.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import os
+from PIL import Image
+
+#API_HOST = os.getenv("API_HOST")
+
+# Setting the wide config for the page
+st.set_page_config(layout="wide")
+#adding marging specs for the main page with css inyection
+margins_css = """
+    <style>
+        .main > div {
+            padding-left: 2rem;
+            padding-right: 2rem;
+            padding-top: 0.5rem;
+        }
+    </style>
+"""
+
+st.markdown(margins_css, unsafe_allow_html=True)
+
+#Title
+st.markdown("""
+    ## Sergio Isla
+    ### Data Scientist""")
+
+col1, col2 = st.columns([1,1])
+
+with col1:
+    ## Safety Map Front
+    st.markdown("""
+    I just finished my Data Science bootcamp at Le Wagon and am eager to continue developing mainly this new path.
+
+I'm an engineer with technical and administrative experience in search of improving the efficiency of the business activities. I achieve my responsibilities through effective prioritization before work in challenging environments, conciliatory attitude, and opportune communication. \n
+
+During the last two weeks of the bootcamp, three mates and I developed an app showing the crimes in Mexico City, plus a map showing a forecast of the crimes in Mexico City during the next 6 months: \n
+[safety-map.streamlit.app](https://safety-map.streamlit.app/) \n
+
+""")
+
+with col2:
+    st.markdown("""
+    : ) \n
+    : ) \n
+    """)
+    #To be transparent from the beginning, you can find the data for our project publicly available [here](https://datos.cdmx.gob.mx/dataset/victimas-en-carpetas-de-investigacion-fgj#:~:text=Descargar-,V%C3%ADctimas%20en%20Carpetas%20de%20Investigaci%C3%B3n%20(completa),-CSV)
+
+
+st.markdown("""  .\n
+
+    Later I developed a map showing the most frequent Ecobici trips from a specific bike station to a specific bike station during March, 2023.
+""")
+
+with open("app/ecobici_flow_map.html", "r") as f:
+    html_content = f.read()
+
+# Display the HTML content
+st.components.v1.html(html_content, width=500,height=500)
